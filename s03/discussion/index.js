dc5c0c52b14729eb1cb12684813dd5e10ecd9422
Kuugang <jakebajo11@gmail.com>
2025-01-23 14:12:19 +0800

    Added solution for the s03 activity

---
+++ s03/discussion/index.js
@@ -0,0 +1,43 @@
+console.log("Hello World");
+console.log("This is our first JS session!");
+
+// Comments
+// to describe written codes
+// Documentation:
+// code readability
+// maintainability
+// collaboration
+
+/*
+  There are two types of comments:
+  1. Single line comments denoted by two slashes
+  2. Multi line comments denoted by a slash and asterisk
+*/
+
+console.log("Tolits");
+
+// Variables;
+// to contain data within a program.
+
+//Variable declaration w/Initialization
+let myName = "Tolits";
+console.log(myName);
+
+//Variable declaration
+let newVariable;
+console.log(newVariable);
+
+// we use assignment(=) operator to assign/ reassign a value
+let myPet = "Bantay";
+console.log(myPet);
+
+//Reference Error
+// console.log(hello);
+// let hello;
+
+//Declare the variable first before using
+// let hello;
+// console.log(hello);
+
+console.log(hello);
+var hello;
