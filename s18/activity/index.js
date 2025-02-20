b50eb86f58414b9c0629c138330aa8c57d64e930
Kuugang <jakebajo11@gmail.com>
2025-02-20 21:49:25 +0800

    s18 activity

---
+++ s18/activity/index.js
@@ -0,0 +1,110 @@
+/* 
+1. In the S18 folder, create an activity folder and an index.html and index.js file inside of it.
+    - Create an index.html file to attach our index.js file
+    - Copy the template provided by your Instructor and paste it in an index.js file.
+    - Update your local sessions git repository and push to git with the commit message of Add template code s18.
+    - Console log the message Hello World to ensure that the script file is properly associated with the html file.
+*/
+console.log("Hello World");
+
+/*
+    
+    You are a treasure hunter and need to organize your treasure map and loot using array mutator methods. Use all the methods (sort(), fill(), copyWithin(), and reverse()) to solve the following tasks:
+
+    2. Start with the array of loot: ["gold", "silver", "bronze", "emerald", "diamond"].
+
+    3. Sort the loot alphabetically.
+
+    4. Reverse the order of the loot.
+
+    5. Copy the last two treasures to the beginning of the array.
+
+    6. Fill the last three positions with "unknown".
+
+    7. Log the final state of the loot array.
+*/
+
+let loot = ["gold", "silver", "bronze", "emerald", "diamond"];
+loot.sort();
+console.log("Sorted loot: ", loot);
+loot.reverse();
+console.log("Reversed loot: ", loot);
+loot.copyWithin(0, loot.length - 2);
+console.log("Copied treasures: ", loot);
+loot.fill("unknown", loot.length - 3);
+console.log("Final loot: ", loot);
+
+/* 
+
+    8. Debug the following code to allow the functions to properly receive and return the correct values and mimic the output.
+
+    - Check syntax of the following code.
+    - Check if value is returned.
+    - Check the parameters and arguments.
+    - Check the if else statements
+    - Check the loop statements
+    - Check if the array methods used are correct.
+
+*/
+
+let numbers = [1, 2, 3, 4, 6, 8, 9, 10, 12, 13, 20, 25];
+
+function findEvenNumbers(arr) {
+  if (arr.length === 0) {
+    return "The array is empty.";
+  }
+
+  let isAllNumbers = arr.every((num) => typeof num == "number");
+
+  if (isAllNumbers == false) {
+    return "The array does not contain all numbers.";
+  }
+
+  let hasNegative = arr.some((num) => num < 0);
+
+  if (hasNegative === true) {
+    return "The array does not contain all positive integers.";
+  }
+
+  return arr.filter((num) => num % 2 == 0);
+}
+
+let messageIfEmptyArr = findEvenNumbers([]);
+console.log("Message if the array is empty:");
+console.log(messageIfEmptyArr);
+
+let messageIfNotAllNumbers = findEvenNumbers([1, 2, 3, "25", 4]);
+console.log("Message if the array does not contain all number data type:");
+console.log(messageIfNotAllNumbers);
+
+let messageIfNotAllPositiveIntegers = findEvenNumbers([1, 2, 3, -25, 4]);
+console.log("Message if the array does not contain all positive integers:");
+console.log(messageIfNotAllPositiveIntegers);
+
+let evenNumbers = findEvenNumbers(numbers);
+console.log("Even numbers in the array:");
+console.log(evenNumbers);
+
+function findIndex(arr, num) {
+  if (Array.isArray(arr) === false) {
+    return "Error: First argument must be an array";
+  }
+
+  if (typeof num !== "number") {
+    return "Error: Second argument must be a number";
+  }
+
+  return arr.indexOf(num);
+}
+
+let messageIfFirstArgumentNotArray = findIndex(
+  { element1: 1, element2: 2, element3: 3 },
+  3,
+);
+console.log(messageIfFirstArgumentNotArray);
+
+let messageIfSecondArgumentNotNumber = findIndex(numbers, "3");
+console.log(messageIfSecondArgumentNotNumber);
+
+let index = findIndex(numbers, 3);
+console.log(index);
