883c4a9ffecbc00b1ed9be83fbea79cf42bd0346
Kuugang <jakebajo11@gmail.com>
2025-03-27 13:50:39 +0800

    s25 discussion

---
+++ s25/discussion/index.js
@@ -0,0 +1,84 @@
+/*
+⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆ 
+⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿ 
+⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀ 
+⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉
+*/
+
+console.log("Hello World");
+
+//JSON stands for Javascript Object Notation
+//JSON is also used in other programming languages hences the name Javascript Object Notation.
+
+// JSON Objects
+
+/*
+    Syntax:
+    {
+        "propertyA": "valueA",
+        "propertyB": "valueB",
+        ...
+    }
+ */
+
+//Example of JSON Object
+
+/*
+ {
+   city: "Quezon City",
+   province: "Metro Manila",
+   country: "Philippines",
+ }
+*/
+
+//Another example of JSON Object with embedded array as its value:
+
+/*
+{
+  cities: [
+    { city: "Quezon City", province: "Metro Manila", country: "Philippines" },
+    { city: "Manila", province: "Metro Manila", country: "Philippines" },
+    { city: "Makati", province: "Metro Manila", country: "Philippines" },
+  ],
+}
+*/
+
+//JSON Methods
+//Stringify method = it converts our JSON Object into a stringified JSON
+
+//JSON.stringify
+let batchesArr = [{ batchName: "Batch X" }, { batchName: "Batch Y" }];
+console.log(typeof batchesArr);
+
+console.log("Result from stringify method: ");
+console.log(typeof JSON.stringify(batchesArr));
+console.log(JSON.stringify(batchesArr));
+
+//Stringified JSON object.
+let data = JSON.stringify({
+  name: "John",
+  age: 31,
+  address: { city: "Manila", country: "Philippines" },
+});
+
+console.log(data);
+
+//JSON.parse
+
+let parsedData = JSON.parse(data);
+console.log("This is after the parse method:");
+console.log(parsedData);
+
+let stringifiedObject = `{"name" : "John", "age" : "31"}`;
+console.log(JSON.parse(stringifiedObject));
